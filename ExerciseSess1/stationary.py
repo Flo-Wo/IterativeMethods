7ce7560a4370b02648b149911db1277a23c4cfa8
Florian Wolf <florian.2.wolf@uni-konstanz.de>
2020-09-23 19:52:57 +0200

    added small tests

--- ExerciseSess1/stationary.py
+++ ExerciseSess1/stationary.py
@@ -72,18 +72,21 @@ def fast_poisson(V, W, mu, lam, b):
     #print("u = {}\n".format(u))
     return(u)
 
-def solver_poisson(u_guess, nu, y_d, f, m, tol=10^-6, maxIter = 100):
+def solver_poisson(u_guess, nu, y_d, f, m, test, tol=10^-6, maxIter = 5):
     A_small = fd_laplace(m, d=1)
     A = fd_laplace(m, d=2)
     
     # get decomposition
     lam, V = np.linalg.eigh(A_small.toarray())
     # solve right hand side using poisson solver
-    right_side = fast_poisson(V, V, lam, lam, f) # =A^-1 *f
-    right_side = y_d - right_side # y_d- A^-1 *f
-    right_side = fast_poisson(V, V, lam, lam, right_side) # = A^-1*(y_d- A^-1 *f)
+    right_side_f = fast_poisson(V, V, lam, lam, f) # =A^-1 *f
+    right_side_f = fast_poisson(V, V, lam, lam, right_side_f) # =A^-2 *f
+    right_side_y_d = fast_poisson(V, V, lam, lam, y_d) # = A^-1 * y_d
+    right_side = right_side_y_d - right_side_f # = A^-1*(y_d- A^-1 *f)
+    print("\nNorm difference right hand sides \n= {}\n".format(np.linalg.norm(right_side - test)))
+    #right_side = test
     
-    #print("shape right side = {}".format(np.shape(right_side)))
+    #print("right side = {}".format(right_side))
     
     # solve system iteratively using the splitting M = A^-2 and N = (-1)*nu*I
     # and the residual/correction form
@@ -99,16 +102,15 @@ def solver_poisson(u_guess, nu, y_d, f, m, tol=10^-6, maxIter = 100):
     
     res = right_side - (eye_nu.dot(u) + u_res_2)
     k = 0
-    print("k = {0}, \n u = {1}\n".format(k,u))
+    print("k = {0}, \nu = {1}\n".format(k,u))
     while np.linalg.norm(res) >= tol and k <= maxIter:
         # update u with formula u_k+1 = u_k + M^-1 r_k with M^-1 = A^2
         u = u + A.dot(A.dot(res))
         # update residual
         u_res = fast_poisson(V, V, lam, lam, u)
         u_res_2 = fast_poisson(V, V, lam, lam, u_res) # = A^-2
-        
-        
         res = right_side - (eye_nu.dot(u) + u_res_2) # residual_k = f - (nu*Id + A^-2)u_k
+        
         # update counter
         k += 1
         print("k = {0}, \n u = {1}\n".format(k,u))
@@ -119,21 +121,35 @@ m = 2
 nu = 0.5
 
 
-y_d = np.ones(m**2)
-# solution is first canonical base vector
-u_sol = np.zeros(m**2)
-u_sol[0] = 1
+y_d = 10*np.ones(m**2)
 
+u_sol = np.ones(m**2)
+
+######## Testing stuff with fixed right hand side ############
+A_small = fd_laplace(m, d=1)
 A = fd_laplace(m, d=2)
 
+lam, V = np.linalg.eigh(A_small.toarray())
+
+u_A_first = fast_poisson(V, V, lam, lam, u_sol)
+
+u_A_second = fast_poisson(V, V, lam, lam, u_A_first)
+
+eye_nu = nu*sparse.identity(m**2) #=nu*I
+
+right_hand_side = eye_nu.dot(u_sol) + u_A_second
+
+######## END ############
+
 f  = (-1)*(nu* A.dot(A.dot(u_sol)) + u_sol - A.dot(y_d))
+#print("f = {}".format(f))
 
 u_guess = np.zeros(m**2)
-u_guess[0] = 1
+#u_guess[0] = 1
 
-u_test, it = solver_poisson(u_guess, nu, y_d, f, m)
+u_test, it = solver_poisson(u_guess, nu, y_d, f, m, test=right_hand_side)
 
-print("u_sol = {}\n".format(u_sol))
+print("\nu_sol = {}\n".format(u_sol))
 
 print("u_test = {}\n".format(u_test))
 
