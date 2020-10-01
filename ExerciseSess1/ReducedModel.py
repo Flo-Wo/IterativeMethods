5067be2a4dea59fc9a9f6a55f7ae93bce2f644a4
Florian Wolf <florian.2.wolf@uni-konstanz.de>
2020-10-01 18:34:25 +0200

    added test file with plot creation

--- ExerciseSess1/ReducedModel.py
+++ ExerciseSess1/ReducedModel.py
@@ -12,17 +12,19 @@ from scipy import sparse
 from scipy.sparse.linalg import cg
 from scipy.sparse.linalg import LinearOperator
 import matplotlib.pyplot as plt
+plt.close('all')
 
 """
 TO DO:
-    - implement fast poisson using the calculated eigenvalues 
-    and eigenvectors
-    - implement poisson solver vectorized using only one loop
-    - plot behaviour of the methods for different m and nu
-    - compare both systems for increasing and decreasing nu
+    + implement fast poisson using the calculated eigenvalues 
+    and eigenvectors and use it to compute its spectral radius
+    (- implement poisson solver vectorized using only one loop)
+    + plot behaviour of the methods for different m and nu
     - find nu for which Jacobi converges
     - compare both solvers to damped jacobi
-    - calculate the conditional number based on nu
+    + calculate the conditional number based on nu
+    
+    - multigrid
 """
 
 
@@ -57,56 +59,86 @@ def fast_poisson(V, W, mu, lam, b):
     u = np.reshape(U, m*m)
     return(u)
 
-def fun1(m,nu):
-    A = fd_laplace(m,d=2)
+def get_system(m,nu):
+    """
+    Generated the left side system
+    
+    (nu*I + A^{-2})*u
+    
+    for a vector u. This is used to construct a linear operator
+    to get a matrix free system
+    """
+    #A = fd_laplace(m,d=2)
     A_small = fd_laplace(m,d=1)
     eye_nu = nu*sparse.identity(m**2)
     lam, V = np.linalg.eigh(A_small.toarray())
-    def fun2(v):
+    def left_side(v):
         sol = fast_poisson(V,V,lam,lam,v)
         sol2 = fast_poisson(V,V,lam,lam,sol)
         return(eye_nu.dot(v)+sol2)
-    return(fun2)
-        
-def solver_poisson_factored(u_guess, nu, y_d, f, m,tol=1e-12):
-    """
-    Solve by A^2 factorized system :
-        
-    (\nu * A^2 + \identity) \cdot u = A\cdot y_d - f 
-    
-    This system is solved matrix free.
-    """
-    #initializing iteration count and residual history
-    num_iters = 0
-    res = []
-    
-    # create callback function to get residual history 
-    # and the iteration counter
-    def callback(xk):
-        nonlocal num_iters
-        num_iters += 1
-        frame = inspect.currentframe().f_back
-        res.append(frame.f_locals['resid'])
-    
-    # init 1D and 2D laplacian matrices
-    #A_small = fd_laplace(m, d=1)
-    A = fd_laplace(m, d=2)
+    return(left_side)
 
-    # calculate the right hand side of problem
-    right_side = A.dot(y_d)-f
+def get_eigenvector(m,k):
+    v = np.zeros(m)
+    for i in range(0,m):
+        v[i] = np.sin(((i+1) * np.pi * (k+1))/(m+1))
+    return(v)
 
-    # define linear operator of the left side for the cg method
-    def left_side(v):
-        result = (nu*(A.dot(A))).dot(v)+(sparse.identity(m**2)).dot(v)
-        return(result)
-    left_side_op = LinearOperator((m**2, m**2), left_side)
+def laplace_small_decomposition(m):
+    """
+    Function to get the eigenvalues and eigenvectors of the
+    small (1D) laplacian matrix using its toeplitz structure
+    """
+    lam = np.zeros(m)
+    V = np.zeros((m,m))
+    for k in range(0,m):
+        lam[k] = (-2) + 2*np.cos((np.pi * (k+1))/(m+1))
+        u = get_eigenvector(m,k)
+        u = 1/np.linalg.norm(u)*u
+        V[:,k] = u
+        
+    return(lam, V)
+
+def get_spectralradius(lam, nu):
+    """
+    Spectral radius for the system
     
-    # solve system using cg method
-    u,info = cg(left_side_op,right_side,x0=u_guess,tol=tol, callback=callback)
+    nu*I + A^{-2}
     
-    return(u,info,num_iters,res)
+    Using the rules from
+    https://de.wikipedia.org/wiki/Kronecker-Produkt#Rechenregeln
+    to get that the eigenvalues of A (2D) are two times the eigenvalues
+    of A (1D)
+    """
+    mu = (1/np.abs(2*lam))**2 #eigenvalues of A^{-2}
+    return(nu + np.max(mu))
+
+def get_spectralradius_factored(lam, nu):
+    """
+    Spectral radius for the system
     
-def solver_poisson(u_guess,nu, y_d, f,m, tol=1e-12):
+    nu*A^2+ I
+    
+    Using the rules from
+    https://de.wikipedia.org/wiki/Kronecker-Produkt#Rechenregeln
+    to get that the eigenvalues of A (2D) are two times the eigenvalues
+    of A (1D)
+    """
+    mu = nu * (np.abs(2*lam))**2
+    return(1 + np.max(mu))
+
+
+def get_conditional_number(lam, nu):
+    mu = (1/np.abs(2*lam))**2 #eigenvalues of A^{-2}
+    return((nu + np.max(mu))/(nu + np.min(mu)))
+
+def get_conditional_number_factored(lam, nu):
+    mu = nu * (np.abs(2*lam))**2
+    return((1 + np.max(mu))/(1 + np.min(mu)))
+    
+
+
+def solver_poisson(u_guess,nu, y_d, f, m, tol=1e-12, disp=False):
     """
     Solve the original system:
     
@@ -132,7 +164,13 @@ def solver_poisson(u_guess,nu, y_d, f,m, tol=1e-12):
     #A = fd_laplace(m, d=2)
     
     # get decomposition
-    lam, V = np.linalg.eigh(A_small.toarray())
+    #lam_2, V_2 = np.linalg.eigh(A_small.toarray())
+    lam, V = laplace_small_decomposition(m)
+    
+    spectral_radius = get_spectralradius(lam, nu)
+    if disp:
+        print("nu = {0}\nspectral radius = {1}\n".format(nu,spectral_radius))
+    
     # calculate the right hand side of the system
     right_side_f = fast_poisson(V, V, lam, lam, f) # =A^-1 *f
     right_side_f = fast_poisson(V, V, lam, lam, right_side_f) # =A^-2 *f
@@ -140,52 +178,61 @@ def solver_poisson(u_guess,nu, y_d, f,m, tol=1e-12):
     right_side = right_side_y_d - right_side_f # = A^-1*(y_d- A^-1 *f)
     
     #construct linear operator
-    op = fun1(m,nu)
+    op = get_system(m,nu)
     operator = LinearOperator((m**2,m**2),op)
     
     # solve the system using the cg method
     u,info = cg(operator,right_side,x0=u_guess,tol=tol,callback=callback)
     return(u,info,num_iters,res)
 
-m = 100
-nu = 1
+        
+def solver_poisson_factored(u_guess, nu, y_d, f, m,tol=1e-12, disp=False):
+    """
+    Solve the by A^2 factorized system :
+        
+    (\nu * A^2 + \identity) \cdot u = A\cdot y_d - f 
+    
+    This system is solved matrix free.
+    """
+    #initializing iteration count and residual history
+    num_iters = 0
+    res = []
+    
+    # create callback function to get residual history 
+    # and the iteration counter
+    def callback(xk):
+        nonlocal num_iters
+        num_iters += 1
+        #if num_iters % 10 == 0:
+        #    print("iter = {}".format(num_iters))
+        frame = inspect.currentframe().f_back
+        res.append(frame.f_locals['resid'])
+    
+    # init 1D and 2D laplacian matrices
+    #A_small = fd_laplace(m, d=1)
+    A = fd_laplace(m, d=2)
+    lam, V = laplace_small_decomposition(m)
 
 
-y_d = 10*np.ones(m**2)
+    # calculate the right hand side of problem
+    right_side = A.dot(y_d)-f
 
-u_sol = np.ones(m**2)
+    # define linear operator of the left side for the cg method
+    def left_side(v):
+        result = (nu*(A.dot(A))).dot(v)+(sparse.identity(m**2)).dot(v)
+        return(result)
+    left_side_op = LinearOperator((m**2, m**2), left_side)
+    
+    spectral_radius = get_spectralradius_factored(lam, nu)
+    if disp:
+        print("nu = {0}\nspectral radius = {1}\n".format(nu,spectral_radius))
+    
+    # solve system using cg method
+    u,info = cg(left_side_op,right_side,x0=u_guess,tol=tol, callback=callback)
+    
+    return(u,info,num_iters,res)
+    
 
-
-A = fd_laplace(m,d=2)
-f  = (-1)*(nu* A.dot(A.dot(u_sol)) + u_sol - A.dot(y_d))
-#print("f = {}".format(f))
-
-u_guess = np.random.rand(m**2)
-
-#u_guess[0] = 1
-
-u_test,info,num_iters,res = solver_poisson_factored(u_guess, nu, y_d, f,m)
-
-x = np.arange(0,num_iters)
-plt.semilogy(x,res,"x-", label="factored")
-
-print("Number of iterations = {}\n".format(num_iters))
-print("||u_sol - u_test|| = {}\n".format(np.linalg.norm(u_test-u_sol)))
-
-print("#------------- Unfacored system -------------#")
-
-u_test,info,num_iters,res = solver_poisson(u_guess, nu, y_d, f,m)
-
-x = np.arange(0,num_iters)
-plt.semilogy(x,res, label="unfactored")
-plt.title("Konvergenzverhalten, nu = {0}, m = {1}".format(nu, m))
-
-print("Number of iterations = {}\n".format(num_iters))
-print("||u_sol - u_test|| = {}\n".format(np.linalg.norm(u_test-u_sol)))
-
-plt.legend(loc="upper right")
-plt.xlabel("number of iteration")
-plt.ylabel("Norm Residual")
-plt.show()
-
+def damped_jacobi():
+    pass
 
